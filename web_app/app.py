efc84531f0b6d6c2b540b3aa5738b6f45d9ac11b
Xavier Nourry <sir@mx-mbp>
2022-01-01 17:21:57 +0100

    wip

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
 import requests
+import mariadb
+import sys
+
+
 
 app = Flask(__name__)
+
+
+def check_activity_in_db(activity):
+    nb_lines = cur.execute(
+    "SELECT * FROM activities WHERE description=(%s)", (activity,))
+    print("nb_lines : " + str(nb_lines))
+    if nb_lines != None:
+        query_affected_lines = cur.execute("UPDATE activities SET nbOccurrences = nbOccurrences + 1 WHERE description=?", activity)
+        conn.commit()
+        print("Update query") 
+    else:
+        query_affected_lines = cur.execute("INSERT INTO activities (description, nbOccurrences) VALUES (%s, %d)", (activity, 1))
+        conn.commit()
+        print("Insert query")
+
 
 @app.route("/")
 def hello_world():
@@ -9,7 +28,26 @@ def hello_world():
     response = requests.get(url)
     data = response.json()
     activity = data["activity"]
-    return "<p>" + activity + "</p>"
+    html = "<h1>Activité du jour</h1><br><h3>" + activity + "</h3>"
+    check_activity_in_db(activity)
+    return html
 
 if __name__ == '__main__':
+    global cur, conn
+    # Connect to MariaDB Platform
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password="mypass",
+            host="127.0.0.1", #"172.17.0.2", # Ou 127.0.0.1, à voir
+            port=3306,
+            database="TP_INFRA"
+
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
     app.run()
